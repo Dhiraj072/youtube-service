a8672791b3bf7e5476df03aa70f86bb3bef1ac1f
Dhiraj <dhirajbhatt072@gmail.com>
2018-09-16 01:29:02 +0800

    Initial commit - project setup

---
+++ src/main/java/com/github/dhiraj072/youtubeservice/youtubeservice/YoutubeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.dhiraj072.youtubeservice.youtubeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/dhiraj072/youtubeservice/youtubeservice/YoutubeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.dhiraj072.youtubeservice.youtubeservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class YoutubeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
