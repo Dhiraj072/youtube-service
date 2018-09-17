9ea4885f4a9e8c5e7434363911ed375a42a6cdcc
Dhiraj <dhirajbhatt072@gmail.com>
2018-09-17 22:19:40 +0800

    Set dev client port, update rest api tests Remove jackson jar since it's not required as of now

--- src/integration-test/java/com/github/dhiraj072/youtubeservice/video/VideoControllerRestAPITest.java
+++ src/integration-test/java/com/github/dhiraj072/youtubeservice/video/VideoControllerRestAPITest.java
@@ -3,15 +3,31 @@ package com.github.dhiraj072.youtubeservice.video;
 import static io.restassured.RestAssured.when;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(Lifecycle.PER_CLASS)
 class VideoControllerRestAPITest {
+
+  @LocalServerPort
+  private int serverPort;
+
+  @BeforeAll
+  void setup() {
+
+    RestAssured.port = serverPort;
+  }
 
   @Test
   void testReturnsAGoodRandomVideo() {
