8aacbb961bd6f096651ccbf9cc00ef2198a65927
Dhiraj <dhirajbhatt072@gmail.com>
2018-12-23 22:48:50 +0800

    Add logging file. AWS only assembles packages now

--- src/main/java/com/github/dhiraj072/yrandom/youtubeservice/YoutubeAuthHelper.java
+++ src/main/java/com/github/dhiraj072/yrandom/youtubeservice/YoutubeAuthHelper.java
@@ -1,6 +1,5 @@
 package com.github.dhiraj072.yrandom.youtubeservice;
 
-import com.github.dhiraj072.yrandom.youtubeservice.video.YoutubeManager;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
